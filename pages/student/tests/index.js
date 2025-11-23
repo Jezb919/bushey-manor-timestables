9b9b4cd0f5e80f752cf49b3a9868fb6fd58258ad
Jezb919 <jez.bogush@gmail.com>
2025-11-23 18:04:00 +0000

    Update index.js

--- pages/student/tests/index.js
+++ pages/student/tests/index.js
@@ -1,20 +1,24 @@
 export default function TestsPage() {
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>Select Your Test</h1>
-      <p>Choose which times table to practise:</p>
+      <h1>Maths Test</h1>
+      <p>This test includes mixed times tables.</p>
 
-      <ul style={{ marginTop: "1rem", fontSize: "18px" }}>
-        <li><a href="/student/tests/2">2 Times Table</a></li>
-        <li><a href="/student/tests/3">3 Times Table</a></li>
-        <li><a href="/student/tests/4">4 Times Table</a></li>
-        <li><a href="/student/tests/5">5 Times Table</a></li>
-        <li><a href="/student/tests/6">6 Times Table</a></li>
-        <li><a href="/student/tests/7">7 Times Table</a></li>
-        <li><a href="/student/tests/8">8 Times Table</a></li>
-        <li><a href="/student/tests/9">9 Times Table</a></li>
-        <li><a href="/student/tests/10">10 Times Table</a></li>
-      </ul>
+      <a
+        href="/student/tests/start"
+        style={{
+          display: "inline-block",
+          padding: "15px 25px",
+          background: "#0070f3",
+          color: "white",
+          borderRadius: "5px",
+          textDecoration: "none",
+          fontSize: "20px",
+          marginTop: "20px",
+        }}
+      >
+        Start Test
+      </a>
     </div>
   );
 }
