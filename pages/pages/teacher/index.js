279b0dd87e198f624d66597e7271abbb873965a0
Jezb919 <jez.bogush@gmail.com>
2025-11-23 14:22:19 +0000

    Delete pages/pages/teacher directory

--- pages/pages/teacher/index.js
+++
@@ -1,8 +0,0 @@
-export default function TeacherLogin() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Teacher Login</h1>
-      <p>This page will let teachers log in and access the dashboard.</p>
-    </div>
-  );
-}
