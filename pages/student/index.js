50d00f5ae40e7465ad347283c478f50b0cfe9436
Jezb919 <jez.bogush@gmail.com>
2025-11-23 14:08:55 +0000

    Create index.js

---
+++ pages/student/index.js
@@ -0,0 +1,8 @@
+export default function StudentLogin() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center", fontFamily: "Arial" }}>
+      <h1>Student Login</h1>
+      <p>Student login page coming soon...</p>
+    </div>
+  );
+}
