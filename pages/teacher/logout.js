2f210e26e8769df65d522c0b337618acadcf1634
Jezb919 <jez.bogush@gmail.com>
2025-12-26 17:50:41 +0000

    Create logout.js

---
+++ pages/teacher/logout.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export default function TeacherLogoutPage() {
+  useEffect(() => {
+    (async () => {
+      await fetch("/api/teacher/logout", { method: "POST" });
+      window.location.href = "/teacher/login";
+    })();
+  }, []);
+
+  return (
+    <div style={{ padding: 30 }}>
+      Logging you out…
+    </div>
+  );
+}
